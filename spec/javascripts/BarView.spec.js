cc4e58fed398637eb55013c98e7b3c315b3c2f57
James A. Rosen <james@zendesk.com>
2012-09-23 00:33:57 -0700

    BarView: add a simple template

--- spec/javascripts/BarView.spec.js
+++ spec/javascripts/BarView.spec.js
@@ -4,4 +4,29 @@ describe('ProgressBars.Bar', function() {
     expect( Em.View.detect(Em.ProgressBars.Bar) ).toBe(true);
   });
 
+  describe('when added to the DOM', function() {
+    var $fixture, view;
+
+    beforeEach(function() {
+      $fixture = $('<div />').appendTo('body');
+      bar = Em.ProgressBars.Bar.create();
+
+      Em.run(function() {
+        bar.appendTo( $fixture );
+      });
+
+      waitsFor(function() {
+        return $fixture.find( bar.$() ).length === 1;
+      });
+    });
+
+    afterEach(function() {
+      $fixture.remove();
+    });
+
+    it('renders a progress bar', function() {
+      expect( $fixture.find('.progress .bar').length ).toBe(1);
+    });
+  });
+
 });
