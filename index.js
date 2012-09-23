0b360c2bd9e3fc445c44af11dfca05e68a852b78
James A. Rosen <james@zendesk.com>
2012-09-23 10:59:46 -0700

    bump version to 0.10.0

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//  Ember-Progressbars.js version 0.9.0
+//  Ember-Progressbars.js version 0.10.0
 //  (c) 2012 James A. Rosen, Zendesk Inc.
 //  Timecop.js is freely distributable under the Apache v2 License
 //  For all details, documentation, and bug reports, see
@@ -7,27 +7,36 @@
 Ember.ProgressBars = Ember.ProgressBars || {};
 
 Ember.ProgressBars.Bar = Ember.View.extend({
-  template: Em.Handlebars.compile('<div class="bar" {{bindAttr class="statusClass"}}></div>'),
+  classNames: 'progress',
+
+  template: function(context) {
+    var statusClass = context.get('statusClass');
+    return "<div class='bar" + (statusClass ? " " + statusClass : '') + "'></div>";
+  },
 
   percent: 0,
   status: undefined,
 
-  init: function() {
-    var classNames = this.get('classNames') || [];
-    classNames.push('progress');
-    this.set('classNames', classNames);
-    return this._super.apply(this, arguments);
-  },
-
-  onPercentChange: function() {
+  percentDidChange: Ember.observer(function() {
     var percent = this.get('percent') || 0;
-    this.$('.bar').css('width', "%@%".fmt(percent) );
-  }.observes('percent'),
+    this.$('.bar').css('width', percent + "%");
+  }, 'percent'),
 
   statusClass: Ember.computed(function() {
     var status = this.get('status');
-    return status == null ? '' : 'bar-%@'.fmt(status);
-  }).property('status').cacheable()
+    return status == null ? '' : 'bar-' + status;
+  }).property('status').cacheable(),
+
+  // This is more annoying than doing the same thing in a template,
+  // but it eliminates a dependency on the Handlebars compiler at
+  // runtime.
+  statusClassWillChange: Ember.beforeObserver(function() {
+    this.$('.bar').removeClass(this.get('statusClass'));
+  }, 'statusClass'),
+
+  statusClassDidChange: Ember.observer(function() {
+    this.$('.bar').addClass(this.get('statusClass'));
+  }, 'statusClass')
 });
 
 
